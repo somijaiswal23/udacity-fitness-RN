0ded46c1c4fc303dac2e11fc62593a471e26b7ed
somijaiswal23 <somi.jaiswal@iiitb.net>
2020-04-17 11:40:15 +1000

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,84 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StatusBar, Platform } from 'react-native';
 import AddEntry from './components/AddEntry';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import History from './components/History';
+import EntryDetail from './components/EntryDetail';
+// import Live from './components/Live';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import reducer from './reducers';
-import History from './components/History'
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { purple, gray, white } from './utils/colors';
+// import { setLocalNotification, setLocalNotificationForToday } from './utils/helpers';
 
-export default function App() {
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
+
+const TabsNavigatorContainer = () => {
   return (
-    <Provider store={createStore(reducer)}>
-      <View style={{flex:1}}>
-        <AddEntry/>
-      </View>
-    </Provider>
-  );
+    <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon:({focused,size,color}) => {
+              let iconName;
+              if (route.name==='History') {
+                iconName = focused ? 'bookmark' : 'bookmark-o';
+              } else if (route.name==='Add Entry') {
+                iconName = focused ? 'pluscircle' : 'pluscircleo';
+              };
+              return route.name==='History' ? <FontAwesome name={iconName} size={size} color={color}/>
+                        : <AntDesign name={iconName} size={size} color={color}/>
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor:purple,
+            inactiveTintColor:gray
+          }}
+        >
+          <Tab.Screen name='History' component={History}/>
+          <Tab.Screen name='Add Entry' component={AddEntry}/>
+          {/* <Tab.Screen name='Live' component={Live} options={{
+            tabBarIcon: ({size,color}) => <Ionicons name={Platform.OS === 'ios' ? 'ios-speedometer' : 'md-speedometer'} size={size} color={color}/>
+          }}/> */}
+    </Tab.Navigator>
+  )
 }
 
+export default function App() {
+  // useEffect(() => {
+  //   let now = new Date();
+  //   // this checks if the time passed 8 PM it will set the reminder notification for tomorrow
+  //   // other that (the time didn't pass 8 PM) it will set the reminder notification for today
+  //   if (now.getHours() > 20){
+  //     setLocalNotification();
+  //   } else if (now.getHours() < 20){
+  //     setLocalNotificationForToday();
+  //   }
+  // },[]);
+  return (
+    <NavigationContainer>
+      <Provider store={createStore(reducer)}>
+      <View style={{flex:1}}>
+        <StatusBar backgroundColor={purple} barStyle='light-content'/>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={TabsNavigatorContainer} options={{headerShown:false}}/>
+          <Stack.Screen 
+            name='Entry Details' 
+            component={EntryDetail}
+            options={{
+              headerStyle:{
+                backgroundColor:purple,
+              },
+              headerTintColor:white,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
+    </Provider>
+    </NavigationContainer>
+  );
+}
--- components/History.js
+++ components/History.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React,{Component} from 'react';
+import {View, Text, StyleSheet, Platform, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import { receiveEntries, addEntry} from '../actions';
 import {timeToString, getDailyReminderValue} from '../utils/helpers';
 import {fetchCalendarResults} from '../utils/api';
-// import UdaciFitnessCalendar from 'udacifitness-calendar'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors';
+import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
+import { AppLoading } from 'expo';
 
 class History extends Component {
+    state={
+        ready:false
+    }
     componentDidMount() {
         const { dispatch } = this.props;
         fetchCalendarResults()
@@ -18,13 +25,24 @@ class History extends Component {
                     }))
                 }
             })
+            .then(() => this.setState(() => ({
+                ready: true
+            })))
     }
 
     renderItem = ({today, ...metrics}, formattedDate, key) => {
         return (
-            <View>
-                {today ? <Text>{JSON.stringify(today)}</Text> :
-                    <Text>{JSON.stringify(metrics)}</Text>                
+            <View style={styles.item}>
+                {today ? 
+                    <View>
+                        <DateHeader date={formattedDate}/>
+                        <Text style={styles.noDataText}>{today}</Text>
+                    </View> 
+                    :
+                    <TouchableOpacity onPress={() => console.log('Pressed!!')}>
+                        <MetricCard metrics={metrics} date={formattedDate} />
+                    </TouchableOpacity>
+                                    
                 }
             </View>
         )
@@ -32,26 +50,54 @@ class History extends Component {
 
     renderEmptyDate(formattedDate) {
         return(
-            <View>
-                <Text>No Data for this day</Text>
+            <View style= {styles.item}>
+                <DateHeader date={formattedDate}/>
+                <Text style={styles.noDataText}>You didn't log any data for this day</Text>
             </View>
         )
     }
 
     render () {
         const  { entries} = this.props;
+        const {ready} = this.state;
+        if(ready === false){
+            return (
+                <AppLoading/>
+            )
+        }
         return(
-            <div></div>
+            <UdaciFitnessCalendar
+                items={entries}
+                renderItem={this.renderItem}
+                renderEmptyDate={this.renderEmptyDate}
+            />
         )
-        // return (
-        //     // <UdaciFitnessCalendar
-        //     //     items={entries}
-        //     //     renderItem={this.renderItem}
-        //     //     renderEmptyDate={this.renderEmptyDate}
-        //     // />
-        // )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: white,
+        borderRadius: Platform.OS === 'ios' ? 16 : 2,
+        padding: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 17,
+        justifyContent: 'center',
+        shadowRadius: 3,
+        shadowOpacity:0.8,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        }
+    },
+    noDataText: {
+        fontSize: 20,
+        paddingTop: 20,
+        paddingBottom: 20
+    }
+})
 
 function mapStateToProps (entries) {
     return {
